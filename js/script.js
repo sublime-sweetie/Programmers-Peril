68a20034bbd04a2991b9719b581fd86054011684
Sublime-Sweetie <faith.Wallace.2@gmail.com>
2022-10-11 22:38:19 -0700

    Timer, high score log, validation

--- js/script.js
+++ js/script.js
@@ -22,12 +22,13 @@ var questions= [
 
 ]
 
+var resetButton = document.querySelector("#reset-button");
 var button =document.querySelector("#game-button");
 var quiz=document.querySelector("#quiz-question");
 var choices=document.querySelector("#quiz-choices");
-// var startingQuestionIndex=0;
+var userScore = 0;
 var currentQuestionIndex=0;
-// choises.innerHTML = '<button>my button</button>'
+// choices.innerHTML = '<button>my button</button>'
 
 function generateQuestion(){
     var currentQuestion = questions[currentQuestionIndex];
@@ -54,31 +55,70 @@ function validation(event){
 
     if (usersChoice === questions[currentQuestionIndex].answer){
         generateQuestion()
+        userScore++
     }else{
 
     // decrease time
-    // display next question.
     generateQuestion()
     }
-    // validating user's answer.
-    // if it is correct
-    // display next question.
-
 
     
 }
 
 button.addEventListener("click", generateQuestion)
-// choices.addEventListener("click", checkA)
+button.addEventListener("click", startTimer)
+
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+        
+
+        if (--timer < 1) {
+            endGame;
+        }
+    }, 1000);
+}
+
+window.load = function() {
+    var twoMinutes = 60 * 2,
+        display = document.querySelector('#time');
+    startTimer(twoMinutes, display);
+};
 
 
 
+  
+function endGame (){
+    alert("Game Over! Your score is ", userScore)
 
+    
+  var highscore = localStorage.getItem("highscore");
+  
+  if(highscore !== null){
+      if (userScore > highscore) {
+        var winnerName
+        prompt("Congrats! You've got the new highscore! Please type your initials!")
+          localStorage.setItem("highscore", winnerName, userScore);      
+      }
+  }
+  else{
+      localStorage.setItem("highscore", winnerName, userScore);
+  }
+console.log(highscore,winnerName)
+    };
 
-
-
-
-
-
-
-
+function resetGame() {
+    userScore=0;
+    generateQuestion;
+    
+  }
+  resetButton.addEventListener("click", resetGame);
